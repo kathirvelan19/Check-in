3a2906602748f5d791691ac6a7f7eaa3eb98ea75
KATHIR VELAN R <kathirvelan655@gmail.com>
2025-09-04 18:57:49 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
+export default defineConfig({
+  base: "/Check-in/",
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+})
 
 export default defineConfig({
   base: "/Check-in/", 
